b4f4ef6918d14b0f6ff374758bad87940761d2ed
Arne Halvorsen <arne.halvorsen@gmail.com>
2022-09-16 09:12:42 +0200

    wip: starting to maybe getting useful

--- CreateCrudCoreSolution.ps1
+++ CreateCrudCoreSolution.ps1
@@ -1,19 +1,37 @@
 $SolutionDirParent = './Test'
 
 $SolutionName = 'TheAmazingThing'
+$StemNameSpace = 'No.Super.Corp'
 
-$Types = Get-content -Path "./Test/Types.txt"
+$Types = Get-Content -Path "./Test/Types.txt"
+$Stack = Get-Content -Path "./Test/Stack.txt"
 
 #
 # Functions that generate intial code
 #
 
-function Write-Interface {
+function Get_Qualified_Namespace {
+    param (
+        $StemNameSpace,
+        $LocalNameSpace
+    )
+    if ($StemNameSpace.Length -gt 0)
+    {
+        return $StemNameSpace + '.' + $LocalNameSpace
+    }
+    return $LocalNameSpace
+}
+
+function Write-Dto-Interface {
     param (
         $Path,
+        $StemNs,
         $Ns,
-        $Name
+        $Name,
+        [Switch]$NoId
     )
+
+    $Ns = Get_Qualified_Namespace -StemNameSpace $StemNs -LocalNameSpace $Ns
 
     $File = Join-Path -Path $Path -ChildPath ('I' + $Name + '.cs')
     if (-not (Test-Path -Path $File))
@@ -23,40 +41,80 @@ function Write-Interface {
         ('{')  | Out-File -FilePath $File -Append
         ($t + 'public interface I' + $Name) | Out-File -FilePath $File -Append
         ($t + '{') | Out-File -FilePath $File -Append
+        if ($NoId -eq $false)
+        {
+            ($t + $t + 'int Id { get; }') | Out-File -FilePath $File -Append
+        }
         ($t + '}') | Out-File -FilePath $File -Append 
         ('} ') | Out-File -FilePath $File -Append
     }
 }
 
-function Write-Class {
+function Write-Dto-Class {
     param (
         $Path,
         $Using,
+        $StemNs,
         $Ns,
         $Name,
-        $Implements
+        $Implements,
+        [switch]$NoId
     )
+
+    $Ns = Get_Qualified_Namespace -StemNameSpace $StemNs -LocalNameSpace $Ns
 
     $File = Join-Path -Path $Path -ChildPath ($Name + '.cs')
     if (-not (Test-Path -Path $File))
     {
         $t = [char]9
-        ('using ' + $Using + ';') | Out-File -FilePath $File
+        ('using ' + $StemNs + '.' + $Using + ';') | Out-File -FilePath $File
         ('') | Out-File -FilePath $File -Append
         ('namespace ' + $Ns) | Out-File -FilePath $File -Append
         ('{')  | Out-File -FilePath $File -Append
         ($t + 'public class ' + $Name + ' : ' + $Implements) | Out-File -FilePath $File -Append
         ($t + '{') | Out-File -FilePath $File -Append
+        if ($NoId -eq $false)
+        {
+            ($t + $t + 'public int Id { get; init; }') | Out-File -FilePath $File -Append
+        }
         ($t + '}') | Out-File -FilePath $File -Append 
         ('} ') | Out-File -FilePath $File -Append
     }
 }
 
-function Write-Crud-Api {
+function Write-Dbo-Class {
     param (
+        $Path,
+        $Using,
+        $StemNs,
         $Ns,
-        $Path
+        $Name
     )
+
+    $Ns = Get_Qualified_Namespace -StemNameSpace $StemNs -LocalNameSpace $Ns
+
+    $File = Join-Path -Path $Path -ChildPath ($Name + '.cs')
+    if (-not (Test-Path -Path $File))
+    {
+        $t = [char]9
+        ('namespace ' + $Ns) | Out-File -FilePath $File
+        ('{')  | Out-File -FilePath $File -Append
+        ($t + 'public class ' + $Name + 'Dbo') | Out-File -FilePath $File -Append
+        ($t + '{') | Out-File -FilePath $File -Append
+        ($t + $t + 'public int Id { get; set; }') | Out-File -FilePath $File -Append
+        ($t + '}') | Out-File -FilePath $File -Append 
+        ('} ') | Out-File -FilePath $File -Append
+    }
+}
+
+function Write-Crud-Abstract-Api {
+    param (
+        $SolutionName,
+        $Path,
+        $StemNs
+    )
+
+    $Ns = Get_Qualified_Namespace -StemNameSpace $StemNs -LocalNameSpace ($SolutionName + '.Specification.Api.Abstraction')
 
     $t = [char]9
 
@@ -121,6 +179,109 @@ function Write-Crud-Api {
 
 }
 
+function Write-Crud-Api {
+    param (
+        $SolutionName,
+        $Name,
+        $Path,
+        $StemNs
+    )
+
+    $Ns = Get_Qualified_Namespace -StemNameSpace $StemNs -LocalNameSpace $SolutionName
+
+    $t = [char]9
+    
+    $File = Join-Path -Path $Path -ChildPath ('I' + $Name + 'Api.cs')
+    if (-not (Test-Path -Path $File))
+    {
+        ('using ' + $Ns + '.Specification.Api.Abstraction;') | Out-File -FilePath $File
+        ('using ' + $Ns + '.Specification.Domain.Model;') | Out-File -FilePath $File -Append
+        ('using ' + $Ns + '.Specification.Domain.Param.' + $Name +';') | Out-File -FilePath $File -Append
+        ('') | Out-File -FilePath $File -Append
+        ('namespace ' + $Ns + '.Specification.Api') | Out-File -FilePath $File -Append
+        ('{') | Out-File -FilePath $File -Append
+
+        $M = 'I' + $Name;
+        $C = 'ICreate' + $Name + 'Param'
+        $R = 'IRead' + $Name + 'Param'
+        $U = 'IUpdate' + $Name + 'Param'
+        $D = 'IDelete' + $Name + 'Param'
+
+        ($t + 'public interface I' + $Name + 'Api : ICrud<' + $M + ', ' + $C + ', ' + $R + ', ' + $U + ', ' + $D + '> {}') | Out-File -FilePath $File -Append
+        ('}') | Out-File -FilePath $File -Append
+    }
+}
+
+function Add-Project-And-Push-Location {
+    param (
+        $Name,
+        $Type,
+        $StemNs,
+        [switch] $Specification,
+        [switch] $Domain
+    )
+
+    $ProjectDir = ($SolutionName + '.' + $Name)
+    if (-not (Test-Path -Path $ProjectDir -PathType Container))
+    {
+        mkdir $ProjectDir
+    }
+    Push-Location $ProjectDir
+
+        if (-not (Test-Path -Path ($ProjectDir + ".csproj")))
+        {
+            $tmp = $StemNameSpace + '.' + $ProjectDir
+            dotnet new $Type -n $tmp -o '.'
+
+            Move-Item -Path ('.\' + $tmp + ".csproj") -Destination ('.\' + $ProjectDir + ".csproj")
+
+            dotnet sln ('../../' + $SolutionName + '.sln') add ($ProjectDir + ".csproj")
+            if ($Type -eq 'classlib')
+            {
+                Remove-Item Class1.cs
+            }
+        }
+
+        if ($Specification)
+        {
+            dotnet add reference ('../' + $SpecificationProjectDir + "/" + $SpecificationProjectDir + ".csproj")
+        }
+        if ($Domain)
+        {
+            dotnet add reference ('../' + $DomainProjectDir + "/" + $DomainProjectDir + ".csproj")
+        }
+    
+    return $ProjectDir
+}
+
+# function Add-IoCConf {
+#     param (
+#         $Name
+#     )
+    
+#     if (-not (Test-Path -Path 'Config' -PathType Container))
+#     {
+#         mkdir 'Config'
+#     }
+#     Push-Location 'Config'
+
+#         $t = [char]9
+    
+#         $File = 'IoCConf.cs'
+#         if (-not (Test-Path -Path $File -PathType Leaf))
+#         {
+#             ('namespace ')
+#             ('public static class IoConf') | Out-File -FilePath $File
+#             ('{') | Out-File -FilePath $File -Append
+#             ($t + 'public static IServiceCollection Configure' + $Name + 'Services(this IServiceCollection services, IConfiguration configuration)')
+
+#             ('}') | Out-File -FilePath $File -Append
+#         }
+
+#     Pop-Location # Config
+
+# }
+
 #
 # Script starts
 #
@@ -149,22 +310,7 @@ Push-Location -Path $SolutionDirParent
         }
         Push-Location 'src' 
 
-            #
-            # ENSURE SPECIFICATION PROJECT
-            #
-            $SpecificationProjectDir = ($SolutionName + '.Specification')
-            if (-not (Test-Path -Path $SpecificationProjectDir -PathType Container))
-            {
-                mkdir $SpecificationProjectDir
-            }
-            Push-Location $SpecificationProjectDir
-
-                if (-not (Test-Path -Path ($SpecificationProjectDir + ".csproj")))
-                {
-                    dotnet new classlib  
-                    dotnet sln ('../../' + $SolutionName + '.sln') add ($SpecificationProjectDir + ".csproj")
-                    Remove-Item Class1.cs
-                }
+            $SpecificationProjectDir = (Add-Project-And-Push-Location -StemNs $StemNameSpace -Name 'Specification' -Type 'classlib')[-1]
 
                 if (-not (Test-Path -Path 'Domain' -PathType Container))
                 {
@@ -182,7 +328,7 @@ Push-Location -Path $SolutionDirParent
                         $Dir = Get-Location
                         foreach ($Name in $Types)
                         {
-                            Write-Interface -Path $Dir -Ns $Ns -Name $Name
+                            Write-Dto-Interface -Path $Dir -StemNs $StemNameSpace -Ns $Ns -Name $Name
                         }
 
                     Pop-Location # Model
@@ -204,10 +350,10 @@ Push-Location -Path $SolutionDirParent
                                 $Ns = ($SpecificationProjectDir + '.Domain.Param.' + $Name)
                                 $Dir = Get-Location
                                 
-                                Write-Interface -Path $Dir -Ns $Ns -Name ('Create' + $Name + 'Param')
-                                Write-Interface -Path $Dir -Ns $Ns -Name ('Read' + $Name + 'Param')
-                                Write-Interface -Path $Dir -Ns $Ns -Name ('Update' + $Name + 'Param')
-                                Write-Interface -Path $Dir -Ns $Ns -Name ('Delete' + $Name + 'Param')
+                                Write-Dto-Interface -Path $Dir -StemNs $StemNameSpace -Ns $Ns -Name ('Create' + $Name + 'Param') -NoId
+                                Write-Dto-Interface -Path $Dir -StemNs $StemNameSpace -Ns $Ns -Name ('Read' + $Name + 'Param')
+                                Write-Dto-Interface -Path $Dir -StemNs $StemNameSpace -Ns $Ns -Name ('Update' + $Name + 'Param')
+                                Write-Dto-Interface -Path $Dir -StemNs $StemNameSpace -Ns $Ns -Name ('Delete' + $Name + 'Param')
                                 
                             Pop-Location # $Name
                         }
@@ -229,35 +375,21 @@ Push-Location -Path $SolutionDirParent
                     Push-Location 'Abstraction'
 
                         $Dir = Get-Location
-                        $Ns = ($SpecificationProjectDir + '.Api.Abstraction')
-                        Write-Crud-Api -Path $Dir -Ns $Ns
+                        Write-Crud-Abstract-Api -StemNs $StemNameSpace -Path $Dir -SolutionName $SolutionName
 
                     Pop-Location # Abstraction
+
+                    $Dir = Get-Location
+                    foreach ($Name in $Types)
+                    {
+                        Write-Crud-Api -StemNs $StemNameSpace -SolutionName $SolutionName -Path $Dir -Name $Name
+                    }
 
                 Pop-Location # API
                 
             Pop-Location # Specification project folder
 
-            #
-            # ENSURE DOMAIN PROJECT
-            #
-            $DomainProjectDir = ($SolutionName + '.Domain')
-            if (-not (Test-Path -Path $DomainProjectDir -PathType Container))
-            {
-                mkdir $DomainProjectDir
-            }
-            Push-Location $DomainProjectDir
-
-                if (-not (Test-Path -Path ($DomainProjectDir + ".csproj")))
-                {
-                    dotnet new classlib  
-
-                    dotnet add reference ('../' + $SpecificationProjectDir + "/" + $SpecificationProjectDir + ".csproj")
-
-                    dotnet sln ('../../' + $SolutionName + '.sln') add ($DomainProjectDir + ".csproj")
-                    Remove-Item Class1.cs
-                }
-                
+            $DomainProjectDir = (Add-Project-And-Push-Location -StemNs $StemNameSpace -Name 'Domain' -Type 'classlib' -Specification)[-1]
 
                 if (-not (Test-Path -Path 'Model'))
                 {
@@ -270,7 +402,7 @@ Push-Location -Path $SolutionDirParent
                     $Dir = Get-Location
                     foreach ($Name in $Types)
                     {
-                        Write-Class -Path $Dir -Using $Using -Ns $Ns -Implements ('I' + $Name) -Name $Name
+                        Write-Dto-Class -Path $Dir -Using $Using -StemNs $StemNameSpace -Ns $Ns -Implements ('I' + $Name) -Name $Name
                     }
 
                 Pop-Location # Model
@@ -292,10 +424,10 @@ Push-Location -Path $SolutionDirParent
                             $Using = $SpecificationProjectDir + '.Domain.Param.' + $Name
                             $Ns = $DomainProjectDir + '.Param.' + $Name
                             
-                            Write-Class -Path $Dir -Using $Using -Ns $Ns -Implements ('IRead' + $Name + 'Param') -Name ('Read' + $Name + 'Param')
-                            Write-Class -Path $Dir -Using $Using -Ns $Ns -Implements ('ICreate' + $Name + 'Param') -Name ('Create' + $Name + 'Param')
-                            Write-Class -Path $Dir -Using $Using -Ns $Ns -Implements ('IUpdate' + $Name + 'Param') -Name ('Update' + $Name + 'Param')
-                            Write-Class -Path $Dir -Using $Using -Ns $Ns -Implements ('IDelete' + $Name + 'Param') -Name ('Delete' + $Name + 'Param')
+                            Write-Dto-Class -Path $Dir -Using $Using -StemNs $StemNameSpace -Ns $Ns -Implements ('ICreate' + $Name + 'Param') -Name ('Create' + $Name + 'Param') -NoId
+                            Write-Dto-Class -Path $Dir -Using $Using -StemNs $StemNameSpace -Ns $Ns -Implements ('IRead' + $Name + 'Param') -Name ('Read' + $Name + 'Param')
+                            Write-Dto-Class -Path $Dir -Using $Using -StemNs $StemNameSpace -Ns $Ns -Implements ('IUpdate' + $Name + 'Param') -Name ('Update' + $Name + 'Param')
+                            Write-Dto-Class -Path $Dir -Using $Using -StemNs $StemNameSpace -Ns $Ns -Implements ('IDelete' + $Name + 'Param') -Name ('Delete' + $Name + 'Param')
                         Pop-Location # Name
                     }
 
@@ -303,7 +435,43 @@ Push-Location -Path $SolutionDirParent
 
             Pop-Location # DomainProjectDir
 
+            if ($Stack.Contains('application-web-api'))
+            {
+                Add-Project-And-Push-Location -StemNs $StemNameSpace -Name 'WebApi' -Type 'webapi' -Specification -Domain
+
+                Pop-Location # WebApi project dir
+
+                Add-Project-And-Push-Location -StemNs $StemNameSpace -Name 'Infrastructure.WebApiClient' -Type 'classlib' -Specification -Domain
+
+                Pop-Location 
+            }
+
+            if ($Stack.Contains('repository-sql'))
+            {
+                Add-Project-And-Push-Location -StemNs $StemNameSpace -Name 'Infrastructure.Repository.SqlDatabase' -Type 'classlib' -Specification -Domain
+
+                dotnet add package Dapper
+
+                if (-not (Test-Path -Path 'Dbo'))
+                {
+                    mkdir 'Dbo'
+                }
+                Push-Location 'Dbo'
+
+                    $Dir = Get-Location
+                    foreach ($Name in $Types)
+                    {
+                        Write-Dbo-Class -Path $Dir -StemNs $StemNameSpace -Ns 'Infrastructure.Repository.SqlDatabase.Dbo' -Name $Name
+                    }
+
+                Pop-Location
+
+                Pop-Location
+            }
+
         Pop-Location # src
+
+        dotnet build
 
     Pop-Location # from solution dir
 
